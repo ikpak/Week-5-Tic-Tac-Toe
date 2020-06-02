dfbb4c64bf2e87c8a94b9f676a0402b5e0ee3145
ikpak <inkha.pak@gmail.com>
2020-06-02 17:08:35 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Board from './components/Board'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+      isXNext: true,
+      history: [],
+      winner: null
+    }
+  }
+
+  setTheState = (obj) => {
+    this.setState(obj)
+  }
+
+  timeTravel = (id) => {
+    this.setTheState(this.state.history[id])
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Tic Tac Toe</h1>
+        <div className="row">
+          <div>
+            <Board {...this.state} setTheState={this.setTheState} />
+          </div>
+          <div>
+            History:
+            {this.state.history.map((item, idx) => {
+              return <div><button onClick={() => this.timeTravel(idx)}>move {idx+1}</button></div>})
+            }
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
